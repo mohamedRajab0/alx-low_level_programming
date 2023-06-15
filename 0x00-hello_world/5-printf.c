95b6d8f4a331cf0b287549b03c2effa290906b53
mohamedRajab0 <elzaimmohamed@gmail.com>
2023-06-15 10:10:39 -0700

    c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *  main - Enter point
+ *
+ *  Description: A C program that print with printf function.
+ *
+ *  return: alwys 0 (Success)
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art, /n");
+return (0);
+}
