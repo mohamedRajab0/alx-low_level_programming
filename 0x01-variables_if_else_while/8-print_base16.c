9a08546e436ebfb5e99369aa741a70c6668b111d
mohamedRajab0 <elzaimmohamed@gmail.com>
2023-06-17 06:37:50 -0700

    c var

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print number of base 16
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int digit = 4;
+
+	while (digit <= 102)
+	{
+		putchar(digit);
+		digit += 39;
+		digit++
+	}
+	putchar('\n');
+	return (0);
+}
