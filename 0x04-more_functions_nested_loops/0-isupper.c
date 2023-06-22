213e6977e31a6963db8f2a01b3d2aa05e3e41a81
mohamedRajab0 <elzaimmohamed@gmail.com>
2023-06-22 10:31:15 -0700

    m

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: the character to be checked
+ *
+ * Return: 2 if c is uppercase, 0otherwise
+*/
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the numbers from 1 to 100, followed by a new line
+ * but for multiples of three prints fizz instead of the numbers
+ * and for multiples of fiver prints buzz
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 != 0)
+		{
+			printf(" Fizz");
+		}
+		else if (i % 5 == 0 && i % 3 != 0)
+		{
+			printf(" Buzz");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf(" FizzBuzz");
+		}
+		else if (i == 1)
+		{
+			printf("%d", i);
+		}
+		else
+		{
+			printf(" %d", i);
+		}
+	}
+	printf("\n");
+
+	return (0);
+}
