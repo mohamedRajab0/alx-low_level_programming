860769ee4cc5c8efe91b99f3f1098f64d5d47dba
mohamedRajab0 <elzaimmohamed@gmail.com>
2023-06-26 06:04:45 -0700

    m

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
+#ifndef FILE_MAIN
+#define FILE_MAIN
 
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -16,4 +14,4 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 
-#endif /* MAIN_H */
+#endif /* FILE_MAIN */
