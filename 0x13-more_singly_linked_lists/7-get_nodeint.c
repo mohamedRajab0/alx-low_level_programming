3961213a1dc7f7007111b1b5ed0c126702c7245f
mohanedRajab0 <elzaimmohamed@gmail.com>
2023-10-23 19:49:43 -0700

    ll

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -18,8 +18,6 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		temp = temp->next;
 		i++;
 	}
-	if (temp)
-		return (temp);
-	else
-		return (NULL);
+
+	return (temp ? temp : NULL);
 }
